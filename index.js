5dcad19afbac1a38e9ae57906f93262bb378658c
Latunde Oluwaferanmi <feranmilatunde@icloud.com>
2020-03-27 23:31:32 +0100

    my first js task

---
+++ index.js
@@ -0,0 +1,11 @@
+let internname =  "Latunde Oluwaferanmi";
+console.log("My name is: " + internname);
+let courses  = ["html","css","js"];
+console.log(courses);
+const data = [];
+for (let i=0; i<200; i++) {
+  if (i % 2 !==0) {
+    data.push(i);
+  }
+}
+console.log(data);
